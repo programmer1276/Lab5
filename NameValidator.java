8aa82a8cc89d660af8321d92d1dc6a554531017e
programmer1276 <139918495+programmer1276@users.noreply.github.com>
2024-05-16 00:02:12 +0300

    Delete NameValidator.java

--- NameValidator.java
+++
@@ -1,8 +0,0 @@
-package Validators;
-
-public class NameValidator implements Validatable<String> {
-    @Override
-    public boolean validate(String value) {
-        return !value.isEmpty() && !value.isBlank() && value != null;
-    }
-}
