b0b7901b52fe57c2fa6fdd6987ceb9027ee555a9
programmer1276 <139918495+programmer1276@users.noreply.github.com>
2024-05-15 23:58:45 +0300

    Delete CoordYValidator.java

--- CoordYValidator.java
+++
@@ -1,8 +0,0 @@
-package Validators;
-
-public class CoordYValidator implements Validatable<Double> {
-    @Override
-    public boolean validate(Double value) {
-        return (value > -79) && (value != null);
-    }
-}
