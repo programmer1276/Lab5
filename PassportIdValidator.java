18c0406465873c8e89baa5718c897b3124b9b10f
programmer1276 <139918495+programmer1276@users.noreply.github.com>
2024-05-16 00:02:35 +0300

    Delete PassportIdValidator.java

--- PassportIdValidator.java
+++
@@ -1,8 +0,0 @@
-package Validators;
-
-public class PassportIdValidator implements Validatable<String> {
-    @Override
-    public boolean validate(String value) {
-        return (value.length() <= 42) && (value.length() >= 4);
-    }
-}
