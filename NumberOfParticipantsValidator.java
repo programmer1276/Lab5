2f0f7849ad821d1fc038b9787d850ba6e00f4f5c
programmer1276 <139918495+programmer1276@users.noreply.github.com>
2024-05-16 00:02:19 +0300

    Delete NumberOfParticipantsValidator.java

--- NumberOfParticipantsValidator.java
+++
@@ -1,8 +0,0 @@
-package Validators;
-
-public class NumberOfParticipantsValidator implements Validatable<Integer> {
-    @Override
-    public boolean validate(Integer value) {
-        return value > 0;
-    }
-}
