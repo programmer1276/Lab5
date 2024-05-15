b3cbbb4335cfb87402498468ccac462068166327
programmer1276 <139918495+programmer1276@users.noreply.github.com>
2024-05-16 00:04:19 +0300

    Delete WeightValidator.java

--- WeightValidator.java
+++
@@ -1,8 +0,0 @@
-package Validators;
-
-public class WeightValidator implements Validatable<Integer> {
-    @Override
-    public boolean validate(Integer value) {
-        return value >= 0;
-    }
-}
